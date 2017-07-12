52100ad0160138736e51471f0017612224a688f7
hoboaki <hoboaki@10106.net>
2017-07-12 22:43:15 +0900

    Parameter対応途中。

---
+++ CilTest/DllToXml/DataSet/Parameter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllToXml.DataSet
+{
+    public class Parameter
+    {
+        public static Parameter Create(ParameterInfo aNative)
+        {
+            var param = new Parameter();
+
+            param.FullName = aNative.ParameterType.FullName;
+
+            return param;
+        }
+
+        //------------------------------------------------------------------------------
+        [System.Xml.Serialization.XmlAttribute("full_name")]
+        public string FullName { get; set; }
+    }
+}
