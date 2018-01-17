e5af9c3fea82aa4c102ebaf611b682aefa87433b
hoboaki <hoboaki@10106.net>
2018-01-17 12:46:58 +0900

    async await テストコード。

---
+++ CilTest/TestLib/AsyncAwait.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestLib
+{
+    /// <summary>
+    /// async await。
+    /// </summary>
+    public static class AsyncAwait
+    {
+        public static async Task PrintAbc()
+        {
+            await Print("A", 10);
+            await Print("B", 1);
+            await Print("C", 5);
+        }
+
+        static async Task Print(string aString, int aDelayMsec)
+        {
+            await Task.Delay(aDelayMsec);
+            Console.WriteLine(aString);
+        }
+
+    }
+}
