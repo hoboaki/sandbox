f459db302bcccdf16f84b064fe792a5ce3ebbf34
hoboaki <hoboaki@10106.net>
2018-07-21 00:38:06 +0900

    refactor: 複数のテストコードを書けるようにするためのコード整理。

--- ElectronTest/HelloWorld/content.js
+++ ElectronTest/HelloWorld/content.js
@@ -1,37 +1,44 @@
-
 window.onload = function() {
-    const content = document.getElementById('content');
-    let text = "";
+    // コンテンツ入れ物
+    let contents = []
 
-    text += "<input id=\"JsonReadTestBtn\" type=\"button\" value=\"JsonReadTestBtn\"/>\n";
-    content.innerHTML = text
-
-    document.querySelector('#JsonReadTestBtn').addEventListener('click', onJsonReadTestBtnClicked)
-}
-
-function onJsonReadTestBtnClicked() {
-    const dialog = require('electron').remote.dialog;
-    const filenames = dialog.showOpenDialog(null, {
-        properties: ['openFile'],
-        title: 'Select a json file',
-        defaultPath: '.',
-        filters: [
-            {name: 'json file', extensions: ['json']}
-        ]
-    });
-    if (filenames.length == 0) {
-        return;
-    }
-
-    const fs = require('fs')
-    const filepath = filenames[0];
-    fs.readFile(filepath, 'utf-8', (err, data) => {
-        if(err){
-            alert("An error ocurred reading the file :" + err.message);
+    // Json 読み込みテスト
+    contents.push({id: "JsonReadTest", label: "JsonReadTest", func: onJsonReadTestBtnClicked})
+    function onJsonReadTestBtnClicked() {
+        const dialog = require('electron').remote.dialog;
+        const filenames = dialog.showOpenDialog(null, {
+            properties: ['openFile'],
+            title: 'Select a json file',
+            defaultPath: '.',
+            filters: [
+                {name: 'json file', extensions: ['json']}
+            ]
+        });
+        if (filenames.length == 0) {
             return;
         }
-        const jsonData = JSON.parse(data);
-        console.log(jsonData);
-        alert("Data length is " + data.length + ".");
+
+        const fs = require('fs')
+        const filepath = filenames[0];
+        fs.readFile(filepath, 'utf-8', (err, data) => {
+            if(err){
+                alert("An error ocurred reading the file :" + err.message);
+                return;
+            }
+            const jsonData = JSON.parse(data);
+            console.log(jsonData);
+            alert("Data length is " + data.length + ".");
+        });
+    }
+
+    // コンテンツ展開
+    const contentElem = document.getElementById('content');
+    let text = "";
+    contents.forEach(function(entry) {
+        text += `<input id=\"${entry["id"]}\" type=\"button\" value=\"${entry["label"]}\"/>\n`;
+    });
+    contentElem.innerHTML = text;
+    contents.forEach(function(entry) {
+        document.querySelector(`#${entry["id"]}`).addEventListener('click', entry["func"])
     });
 }
