8f699e2cbc26c603de3d2296e5ea2080ea19cb73
hoboaki <hoboaki@10106.net>
2018-07-21 16:54:41 +0900

    feat: Gridテスト追加中。

--- ElectronTest/HelloWorld/content.js
+++ ElectronTest/HelloWorld/content.js
@@ -1,3 +1,6 @@
+const $ = require('jquery');
+const Grid = require('editable-grid');
+
 window.onload = function() {
     // コンテンツ入れ物
     let contents = []
@@ -30,15 +33,57 @@ window.onload = function() {
             alert("Data length is " + data.length + ".");
         });
     }
+    // Gridテスト
+    contents.push({id: "GridTest", label: "GridTest", func: onGridTestBtnClicked})
+    function onGridTestBtnClicked() {
+        let el = $('#content');
+
+        var fixedHeight = $('<div style="height: 300px"></div>');
+        el.append(fixedHeight);
+
+        var data = [];
+        for (var i = 0  ; i < 20; i++) {
+            data.push({
+                id: 'id-' + i,
+                a: 'a',
+                b: 'b',
+                c: 'c'
+            });
+        }
+
+        var grid = new Grid({
+            el: fixedHeight,
+            columns: [
+                {
+                    id: 'a',
+                    title: 'a',
+                    width: '33.3%'
+                },
+                {
+                    id: 'b',
+                    title: 'b',
+                    width: '33.3%'
+                },
+                {
+                    id: 'c',
+                    title: 'c',
+                    width: '33.3%'
+                }
+            ],
+            data: data
+        });
+        grid.render();
+    }
 
     // コンテンツ展開
-    const contentElem = document.getElementById('content');
+    const menuElem = document.getElementById('menu');
     let text = "";
     contents.forEach(function(entry) {
         text += `<input id=\"${entry["id"]}\" type=\"button\" value=\"${entry["label"]}\"/>\n`;
     });
-    contentElem.innerHTML = text;
+    menuElem.innerHTML = text;
     contents.forEach(function(entry) {
         document.querySelector(`#${entry["id"]}`).addEventListener('click', entry["func"])
     });
 }
+
