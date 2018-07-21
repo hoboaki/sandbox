8eebacaceb913c3e183656595a8c81fdde10db48
hoboaki <hoboaki@10106.net>
2018-07-21 23:06:08 +0900

    feat: コンテキストメニューテスト

--- ElectronTest/HelloWorld/content.js
+++ ElectronTest/HelloWorld/content.js
@@ -1,7 +1,23 @@
 const $ = require('jquery');
 const Grid = require('editable-grid');
+const remote = require('electron').remote;
+const Menu = remote.Menu;
+const MenuItem = remote.MenuItem;
 
 window.onload = function() {
+    // MenuBarテスト（TitleBarの上でのみ反応）
+    {        
+        let menu = new Menu();
+        menu.append(new MenuItem({ label: 'MenuItem1', click: function() { console.log('item 1 clicked'); } }));
+        menu.append(new MenuItem({ type: 'separator' }));
+        menu.append(new MenuItem({ label: 'MenuItem2', type: 'checkbox', checked: true }));
+        
+        document.getElementById('titleBar').addEventListener('contextmenu', function (e) {
+            e.preventDefault();
+            menu.popup(remote.getCurrentWindow());
+        }, false);
+    }
+
     // コンテンツ入れ物
     let contents = []
 
