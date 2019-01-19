e7df37299a97235767bf2c24ab7d78394dee5ba0
hoboaki <hoboaki@10106.net>
2019-01-20 00:02:49 +0900

    feat: ポートも設定ファイルに抽出。

--- Vagrant/vagrant-practice/host-root/app/nginx/cmd.sh
+++ Vagrant/vagrant-practice/host-root/app/nginx/cmd.sh
@@ -11,16 +11,26 @@ source "${base_dir}/env.sh"
 for OPT in "$@"
 do
   case $OPT in
+    # イメージをビルド＆コンテナを起動
     'run' )
       echo \# ${OPT}
+
+      opt_port=""
+      for p in ${DOCKER_PORTS[@]}; do
+        opt_port="${opt_port} -p ${p}"
+      done
+
       docker build . -t ${DOCKER_IMAGE_NAME}
-      docker run -d -p 80:80 -p 443:443 --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
+      docker run -d ${opt_port} --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
       ;;
+
+    # 起動中のコンテナを停止＆削除
     'stop' )
       echo \# ${OPT}
       docker stop $(docker ps -a -q --filter ancestor=${DOCKER_CONTAINER_NAME} --format="{{.ID}}")
     docker rm $(docker ps -a -q --filter ancestor=${DOCKER_CONTAINER_NAME} --format="{{.ID}}")
       ;;
+      
   esac
   shift
 done
