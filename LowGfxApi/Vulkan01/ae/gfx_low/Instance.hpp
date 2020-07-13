bd6b2ab43c56864f0966f0c715e95ee3bffcba66
hoboaki <hoboaki@10106.net>
2020-07-14 00:16:51 +0900

    feat: Instance クラスのファイルを追加。

---
+++ LowGfxApi/Vulkan01/ae/gfx_low/Instance.hpp
@@ -0,0 +1,13 @@
+// 文字コード：UTF-8
+#pragma once
+
+//------------------------------------------------------------------------------
+namespace ae {
+namespace gfx_low {
+
+/// グラフィックスライブラリインスタンス。
+class Instance {};
+
+}  // namespace gfx_low
+}  // namespace ae
+// EOF
